d2b0806b912061b7d4aad0e7308d62b0c942c9cb
l0gan <l0gan@users.noreply.github.com>
2015-11-13 14:31:39 -0500

    Updates to quietKali.py - getting there

--- quietKali.py
+++ quietKali.py
@@ -5,6 +5,14 @@ import string
 import random
 from argparse import ArgumentParser
 
+# quietKali is a script to make setting up your Kali instance for a red team engagement simpler.
+# Ever been on an engagement and kali attempted to do an update, getting caught in web proxy logging?
+# Ever been on an engagement and your kali box was named 'kali' or had the word kali in the name and it get caught by DHCP logging?
+# quietKali will help quiet your computer to keep it from giving you away.
+# quietKali also will allow you to randomize your hostname, and MAC address, so you are harder to detect.
+# Coming soon! quietKali will attempt to match your MAC address to your hostname type. If you are using a printer hostname, 
+# quietKali will set your MAC to match.
+
 parser = ArgumentParser(description='Modify your Kali machine to look more like a normal machine on the network and keep from giving yourself away to the defenders.')
 parser.add_argument('--on', action='store_true', help='Enable quietKali. This will modify your machine to keep from connecting to Kali servers.')
 parser.add_argument('--off', action='store_true', help='Disable quietKali. This will remove settings set when turning on quietKali and allow you to connect to Kali servers again. quietKali must be off to run apt-get update and install.')
@@ -35,13 +43,47 @@ def quietKali():
 
 def revert():
 	print("Reverting machine...Please wait")
-	print("Reverting MAC Address...")
-        inter=raw_input("What interface should be reset?:")
-	os.system("ifconfig " + inter + " down")
-        os.system("macchanger -p " + inter)
-        print("Your MAC address has been reset.")
-        os.system("ifconfig " + inter + " up")
+	macChanged=raw_input("Did you change your MAC address? (Y/N):")
+	if macChanged.lower() == "y":
+		print("Reverting MAC Address...")
+        	inter=raw_input("What interface should be reset?:")
+		os.system("ifconfig " + inter + " down")
+        	os.system("macchanger -p " + inter)
+        	print("Your MAC address has been reset.")
+        	os.system("ifconfig " + inter + " up")
+	hostnameChanged=raw_input("Did you change your hostname? (Y/N):")
+	if hostnameChanged.lower() == "y":
+		if os.path.isfile("/etc/hostname.orig"):
+			os.system("mv /etc/hostname.orig /etc/hostname")
+        		os.system("service hostname.sh")
+        		print("Hostname has been changed back to your original name.")
+		else:
+			hostnameNew=raw_input("I did not find the backup file '/etc/hostname.orig'. Please enter the name you want to change the hostname to:")
+			f=open("/etc/hostname","w")
+			f.write(hostnameNew + "\n")
+			f.close
+			os.system("service hostname.sh") #Not sure why, but this isn't working like it should.... 
+			print("Hostname has been set.")
+	print("Turning off quietKali...")
+	quietKaliOff()
 
+def quietKaliOn():
+	print("Backing up your hosts file...")
+	os.system("cp /etc/hosts /etc/hosts.orig")
+	os.system("echo '127.0.0.1    kali    kali.com    kali.org security.kali.org    http.kali.org    update.kali.org' >> /etc/hosts")
+	os.system("iptables -A OUTPUT -d 192.124.249.10 -j DROP")
+	os.system("iptables -A OUTPUT -d 192.99.200.113 -j DROP")
+
+
+def quietKaliOff():
+	print("Restoring your hosts file...")
+	if os.path.isfile("/etc/hosts.orig"):
+		os.system("mv /etc/hosts.orig /etc/hosts")
+	else:
+		print("The backup file '/etc/hosts.orig' is missing. Cannot revert hosts file. Please remove manually the line that says:\n127.0.0.1    kali    kali.com    kali.org security.kali.org    http.kali.org    update.kali.org")
+	print("Reverting iptables rules...")
+	os.system("iptables -D OUTPUT -d 192.124.249.10 -j DROP")
+	os.system("iptables -D OUTPUT -d 192.99.200.113 -j DROP")
 
 def macChange():
 	print("Changing MAC Address...")
@@ -56,7 +98,8 @@ def hostnameChange():
 	if hostnameLower == "server":
 		print("You selected a Server hostname. Generating....")
 		number=id_generator()
-		hostnameNew="Server-" + number
+		servername=serverBaseName()
+		hostnameNew=(servername.rstrip("\n") + "-" + number)
 	elif hostnameLower == "workstation":
 		print("You selected a Workstation hostname. Generating....")
 		number=id_generator()
@@ -88,7 +131,7 @@ def hostnameChange():
 def id_generator(size=4, chars=string.digits):
                 return ''.join(random.choice(chars) for _ in range(size))
 
-#def serverBaseName():
-#	return random.choice(open("servers.txt").readlines())	
+def serverBaseName():
+	return random.choice(open("servers.txt").readlines())	
 
 quietKali()
